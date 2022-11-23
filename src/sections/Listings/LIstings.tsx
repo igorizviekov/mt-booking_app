4e0c46867dafcd4c00c790a11db4e07b5bf83b95
Dmytro Iudin <dmtry.iudin@gmail.com>
2022-11-23 14:39:38 +0200

    feat: antd added to listings

--- src/sections/Listings/LIstings.tsx
+++ src/sections/Listings/LIstings.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { server, useQuery } from '../../lib/api/index';
 import { Listing } from './types';
+import { List, Button, Spin, Row, Col, Alert, Avatar, Skeleton } from 'antd';
 
 interface ListingsProps {
   title: string;
@@ -14,23 +15,65 @@ export const Listings: React.FC<ListingsProps> = ({ title }) => {
     refetch();
   };
 
-  const listings = data
-    ? data.map((elem) => {
-        return (
-          <div key={elem.id}>
-            <h2>{elem.title}</h2>
-            <button onClick={() => deleteListing(elem.id)}>delete</button>
-          </div>
-        );
-      })
-    : null;
+  // const listings = data ? data.map() : null;
 
   if (error) {
-    return <h1>{'Error'}</h1>;
+    return (
+      <Alert
+        message='Error'
+        type='error'
+        description='Something went wrong, try again later ￣\_(0_o)_/￣'
+        showIcon></Alert>
+    );
   }
 
   if (loading) {
-    return <h1>loading...</h1>;
+    return (
+      <>
+        <Row
+          style={{ height: '100vh' }}
+          justify='space-around'
+          align='middle'>
+          <Col>
+            <Spin size='large' />
+          </Col>
+        </Row>
+      </>
+    );
   }
-  return <>{listings}</>;
+  return (
+    <>
+      <h2>{title}</h2>
+      <List
+        bordered
+        dataSource={data ? data : []}
+        renderItem={(elem) => {
+          return (
+            <List.Item
+              actions={[
+                <Button onClick={() => deleteListing(elem.id)}>Delete</Button>,
+              ]}>
+              <Skeleton
+                avatar
+                active
+                loading={false}>
+                <List.Item.Meta
+                  key={elem.id}
+                  title={elem.title}
+                  description={elem.address}
+                  avatar={
+                    <Avatar
+                      src={elem.image}
+                      shape='square'
+                      size={80}
+                    />
+                  }
+                />
+              </Skeleton>
+            </List.Item>
+          );
+        }}
+      />
+    </>
+  );
 };
