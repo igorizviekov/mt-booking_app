b0640ecb59e66c95da4a773d7359b4cdb0096d71
Dmytro Iudin <dmtry.iudin@gmail.com>
2022-11-25 18:13:34 +0200

    docs: added documentation to interfaces

--- src/components/Typography/types.ts
+++ src/components/Typography/types.ts
@@ -1,3 +1,9 @@
+/**
+ * Types for typography component
+ * @param {number} size - Size of the text (pixels)
+ * @param {string} label - Text content of the compopnent
+ */
+
 export interface ITypographyProps extends Partial<HTMLHeadingElement> {
   size: number;
   label: string;
--- src/sections/Listings/types.ts
+++ src/sections/Listings/types.ts
@@ -1,3 +1,15 @@
+/**
+ * Types for typography component
+ * @param {id} string - ID of the listing
+ * @param {title} string - Title of the listing
+ * @param {image} string - Image of the listing
+ * @param {address} string - Address of the listing item
+ * @param {price} number - Price to order/buy listing item
+ * @param {numOfGuests} number -Number of guests listing item can contain
+ * @param {numOfBeds} number - Number of beds listing item can contain
+ * @param {rating} number - Rating of the listing item
+ */
+
 export interface IListingProps {
   id: string;
   title: string;
