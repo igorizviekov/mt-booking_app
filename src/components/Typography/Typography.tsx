32039294d6534ccbdc05c306ad374bcc95efda94
Dmytro Iudin <dmtry.iudin@gmail.com>
2022-11-28 11:51:35 +0200

    refactor: updated interface for typography

--- src/components/Typography/Typography.tsx
+++ src/components/Typography/Typography.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { ITypographyProps } from './typography.types';
 
-export const Typography: React.FC<ITypographyProps> = ({ size, label }) => {
+export const Typography: React.FC<ITypographyProps> = (props) => {
+  // const additionalProps = (propsObj: ITypographyProps) => {
+  //   delete propsObj.size;
+  //   delete propsObj.label;
+
+  //   return propsObj;
+  // };
+
   return (
-    <h2
+    <span
       className='mb-4
       text-text-color-1
       dark:text-white
@@ -11,8 +18,9 @@ export const Typography: React.FC<ITypographyProps> = ({ size, label }) => {
       md:ml-8
       lg:ml-24
       xl:ml-34'
-      style={{ fontSize: `${size}px` }}>
-      {label}
-    </h2>
+      style={{ fontSize: `${props.size}px` }}
+      {...props}>
+      {props.label}
+    </span>
   );
 };
--- src/components/Typography/typography.types.d.ts
+++ src/components/Typography/typography.types.d.ts
@@ -1,10 +1,13 @@
+import { HTMLAttributes } from 'react';
+
 /**
  * Types for typography component
  * @param {number} size - Size of the text (pixels)
  * @param {string} label - Text content of the compopnent
  */
 
-export interface ITypographyProps extends Partial<HTMLHeadingElement> {
-  size: number;
-  label: string;
+export interface ITypographyProps extends HTMLAttributes<HTMLSpanElement> {
+  size?: number;
+  label?: string;
+  // [propName: string]: any;
 }
