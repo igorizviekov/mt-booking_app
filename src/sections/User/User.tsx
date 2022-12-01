fc7924fd127bdcf02378e2037b5ea8d87788c997
Dmytro Iudin <dmtry.iudin@gmail.com>
2022-12-01 11:18:57 +0200

    feat: added user data to user page

--- src/sections/User/User.tsx
+++ src/sections/User/User.tsx
@@ -1,3 +1,30 @@
+import { Avatar, Card, Space, Divider } from 'antd';
+import { useStoreState } from '../../store/hooks';
+
 export const User = () => {
-  return <h1>User</h1>;
+  const { authData } = useStoreState((state) => state.auth);
+  return (
+    <Card>
+      <Space
+        direction='vertical'
+        size='large'
+        style={{ display: 'flex' }}>
+        <Avatar
+          size={64}
+          icon={<img src={authData.profileObj.imageUrl} />}
+        />
+        <Divider plain>
+          <span className='text-2xl'>Details</span>
+        </Divider>
+        <div>
+          <div className='text-lg font-bold'>
+            Name: {authData.profileObj.name}
+          </div>
+          <div className='text-lg font-bold'>
+            Email: {authData.profileObj.email}
+          </div>
+        </div>
+      </Space>
+    </Card>
+  );
 };
