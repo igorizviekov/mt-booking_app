7a6bdb61185db4c9df3598b3064ef0911616da27
Dmytro Iudin <dmtry.iudin@gmail.com>
2022-12-05 18:09:03 +0200

    feat: persisting added

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,20 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'antd/dist/reset.css';
 import './styles/index.scss';
-import { setupStore } from './store/index';
+import { store } from './store/index';
 import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 
-const store = setupStore();
+const persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <Provider store={store}>
-    <App />
+    <PersistGate
+      loading={null}
+      persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
 );
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,30 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import authReducer from './reducers/AuthSlice';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer } from 'redux-persist';
 
 const rootReducer = combineReducers({
   auth: authReducer,
 });
 
-export const setupStore = () => {
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const setupStore = () => {
   return configureStore({
-    reducer: rootReducer,
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: false,
+      }),
   });
 };
+
+export const store = setupStore();
 
 export type RootState = ReturnType<typeof rootReducer>;
 export type AppStore = ReturnType<typeof setupStore>;
