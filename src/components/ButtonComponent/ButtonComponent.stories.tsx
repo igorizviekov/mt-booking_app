ffbd573e5635a7109d327a9b460f8810e0c80e43
Dmytro Iudin <dmtry.iudin@gmail.com>
2022-12-05 10:53:02 +0200

    docs: added storybook to button

---
+++ src/components/ButtonComponent/ButtonComponent.stories.tsx
@@ -0,0 +1,20 @@
+import { ButtonComponent } from './ButtonComponent';
+import { Meta, Story } from '@storybook/react';
+
+export default {
+  title: 'Button',
+  component: ButtonComponent,
+} as Meta;
+
+const Template: Story = (args: any) => (
+  <ButtonComponent {...args}>Hello World!</ButtonComponent>
+);
+export const Primary = Template.bind({});
+Primary.args = {
+  primary: true,
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  secondary: true,
+};
