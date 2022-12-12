b8859b8415977f57a40ac90b2d59f190754a918b
Dmytro Iudin <dmtry.iudin@gmail.com>
2022-12-12 17:37:54 +0200

    fix: added responsive design to main page

--- src/components/HomeHero/HomeHero.tsx
+++ src/components/HomeHero/HomeHero.tsx
@@ -12,7 +12,7 @@ export const HomeHero: React.FC<HomeHeroProps> = ({ onSearch }) => {
           Find a place you will love to stay at
         </h2>
         <Search
-          className='w-1/2 shadow-md'
+          className='sm:w-1/2 shadow-md'
           style={{ borderRadius: '10px' }}
           size='large'
           enterButton
@@ -20,24 +20,27 @@ export const HomeHero: React.FC<HomeHeroProps> = ({ onSearch }) => {
           onSearch={onSearch}
         />
       </div>
-      <div className='flex items-center justify-between mt-20 space-x-5'>
+      <div className='flex items-center md:justify-between mt-20 space-x-5 justify-center'>
         <HomeHeroCard
           href='dubai'
           title='Dubai'
           image='https://i2.wp.com/www.archute.com/wp-content/uploads/2016/01/beautiful-view-of-burj-khalifa.jpg'
         />
         <HomeHeroCard
+          classList='hidden xl:block'
           href='los angeles'
           title='Los Angeles'
           image='https://thumbs.dreamstime.com/b/godzina-szczytu-ruch-drogowy-baltimore-maryland-52268675.jpg'
         />
         <HomeHeroCard
+          classList='hidden lg:block'
           href='london'
           title='London'
           // eslint-disable-next-line max-len
           image='https://images.unsplash.com/photo-1545853332-147d5073187e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'
         />
         <HomeHeroCard
+          classList='hidden md:block'
           href='toronto'
           title='Toronto'
           image='https://de-academic.com/pictures/dewiki/84/Toronto_-_ON_-_CN_Tower_Illuminiert.jpg'
