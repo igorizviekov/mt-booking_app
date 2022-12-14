d546c1202b56cf28d438cbac749dd8a779b6d840
Dmytro Iudin <dmtry.iudin@gmail.com>
2022-12-14 13:21:10 +0200

    feat: added drag and drop component

--- src/sections/Host/Host.tsx
+++ src/sections/Host/Host.tsx
@@ -3,12 +3,13 @@ import { Content } from 'antd/es/layout/layout';
 import { BsHouseDoorFill } from 'react-icons/bs';
 import { MdApartment } from 'react-icons/md';
 import { BsPlusLg } from 'react-icons/bs';
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 export const Host = () => {
-  const [image, setImage] = useState<File | null>(null);
+  const [image, setImage] = useState<string | ArrayBuffer | null>(null);
+  const [drag, setDrag] = useState<boolean>(false);
 
-  const toBase64 = (file: File) =>
+  const toBase64 = (file: File): Promise<string | ArrayBuffer | null> =>
     new Promise((resolve, reject) => {
       const reader = new FileReader();
       reader.readAsDataURL(file);
@@ -16,10 +17,26 @@ export const Host = () => {
       reader.onerror = (error) => reject(error);
     });
 
+  const dragStartHandler = (e: any) => {
+    e.preventDefault();
+    setDrag(true);
+  };
+
+  const dragLeaveHandler = (e: any) => {
+    e.preventDefault();
+    setDrag(false);
+  };
+
+  const dropHandler = async (e: any) => {
+    e.preventDefault();
+    const files = [...e.dataTransfer.files];
+    const fileBase64 = await toBase64(files[0]);
+    setImage(fileBase64);
+    setDrag(false);
+  };
+
   const onFinish = async (values: any) => {
-    if (image) {
-      values.image = await toBase64(image);
-    }
+    values.image = image;
     console.log('Success:', values);
   };
 
@@ -82,21 +99,38 @@ export const Host = () => {
           rules={[{ required: true, message: 'This field can not be empty!' }]}>
           <InputNumber />
         </Form.Item>
-        <div className='flex flex-col items-center w-28'>
-          <h4 className='text-center mb-2'>Drop your avatar here</h4>
-          <div
-            className='w-28
-        h-28
-        bg-gray-100
-        border-xl
-        border-2
-        border-gray-200
-        rounded
-        flex
-        items-center
-        justify-center'>
-            <BsPlusLg size='2em' />
+        <div className='flex items-center'>
+          <div className='flex flex-col items-center w-28'>
+            <h4 className='text-center mb-2'>Drop your avatar here</h4>
+            <div
+              className='
+              w-28
+              h-28
+              bg-gray-100
+              border-xl
+              border-2
+              border-gray-200
+              rounded
+              flex
+              items-center
+              justify-center'
+              onDragStart={dragStartHandler}
+              onDragLeave={dragLeaveHandler}
+              onDragOver={dragStartHandler}
+              onDrop={dropHandler}>
+              {drag ? (
+                <span className='animate-bounce'>Drop here</span>
+              ) : (
+                <BsPlusLg size='2em' />
+              )}
+            </div>
           </div>
+          {image ? (
+            <img
+              src={image.toString()}
+              className='h-40 ml-10 rounded'
+            />
+          ) : null}
         </div>
         <Button
           htmlType='submit'
