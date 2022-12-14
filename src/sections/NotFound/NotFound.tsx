0eb5f4a05913bcd6016a8e7b68fe271d84386aa2
Dmytro Iudin <dmtry.iudin@gmail.com>
2022-12-14 17:03:15 +0200

    feat: 404 page updated

--- src/sections/NotFound/NotFound.tsx
+++ src/sections/NotFound/NotFound.tsx
@@ -1,5 +1,27 @@
+import { Button } from 'antd';
+import { Content } from 'antd/es/layout/layout';
 import React from 'react';
 
 export const NotFound = () => {
-  return <h1>NotFound</h1>;
+  return (
+    <Content className='p-10 flex items-center justify-center'>
+      <div className='w-1/2 flex flex-col items-center space-y-10 mt-20'>
+        <h1 className='text-5xl bold'>Page not found</h1>
+        <span className='text-xl'>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id
+          consequuntur ipsam adipisci nulla illo aut non numquam corrupti
+          voluptate exercitationem veniam tenetur vitae, eum consequatur illum,
+          repellendus qui quos laudantium!
+        </span>
+        <div className='w-full flex justify-around'>
+          <Button
+            type='primary'
+            href='/'>
+            Visit Homepage
+          </Button>
+          <Button>Contact us</Button>
+        </div>
+      </div>
+    </Content>
+  );
 };
