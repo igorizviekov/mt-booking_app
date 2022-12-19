ca580aefb2e3d72b8809d36c59b119b0b200576d
Dmytro Iudin <dmtry.iudin@gmail.com>
2022-12-19 14:35:19 +0200

    docs: added storybook

--- src/components/DragAndDrop/DragAndDrop.stories.tsx
+++ src/components/DragAndDrop/DragAndDrop.stories.tsx
@@ -2,7 +2,7 @@ import { DragAndDrop } from './DragAndDrop';
 import { Meta, Story } from '@storybook/react';
 
 export default {
-  title: 'DragAndDrop',
+  title: 'form/DragAndDrop',
   component: DragAndDrop,
 } as Meta;
 
@@ -10,7 +10,18 @@ const Template: Story = (args: any) => <DragAndDrop {...args} />;
 export const Primary = Template.bind({});
 Primary.args = {
   image: null,
-  setImage: (a: string | ArrayBuffer | null) => null,
+  setImage: (a: null) => null,
+  maxFileSize: 1337,
+  fileTypes: [],
+  required: false,
+  label: 'Drop image here',
+};
+
+export const WithImage = Template.bind({});
+WithImage.args = {
+  image:
+    'https://imgsv.imaging.nikon.com/lineup/coolpix/p/p7000/img/sample/img_02_b.jpg',
+  setImage: (a: null) => null,
   maxFileSize: 1337,
   fileTypes: [],
   required: false,
