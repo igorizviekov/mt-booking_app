bb0181a066272b711a8cbc802c9eacb5057bc1a3
Dmytro Iudin <dmtry.iudin@gmail.com>
2022-12-26 10:58:27 +0200

    refactor: mock data updated

--- src/mock-data/houses.js
+++ src/mock-data/houses.js
@@ -3,9 +3,7 @@ const data = [
   {
     address:
       'Baker Street, Marylebone Road, Marylebone, Westminster, Greater London, England, NW1 5LD, United Kingdom',
-    id: '1',
-    image:
-      'https://thespaces.com/wp-content/uploads/2016/06/Sherlock-flat-bbc.jpg',
+    id: '4',
     numOfBeds: 2,
     numOfGuests: 2,
     price: 15,
@@ -22,8 +20,6 @@ const data = [
     address:
       'Meiko, Keskinen Kirkkonummi, Kirkkonummi, Helsinki sub-region, Uusimaa, Southern Finland, Mainland Finland, Finland',
     id: '2',
-    image:
-      'https://housely.com/wp-content/uploads/2016/10/ConnecticutHouse_01.jpg',
     numOfBeds: 3,
     numOfGuests: 6,
     price: 30,
@@ -33,13 +29,10 @@ const data = [
     description:
       'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
   },
-
   {
     address:
       'Winnetka, Los Angeles, Los Angeles County, California, 91306, United States',
     id: '3',
-    image:
-      'https://www.chicagotribune.com/resizer/0m2LwOyUvSRKZKbPObPuVQKRzq0=/1200x0/top/arc-anglerfish-arc2-prod-tronc.s3.amazonaws.com/public/ZCJ77ZAR7FE2VBDQOTEC24ESSI.jpg',
     numOfBeds: 3,
     numOfGuests: 10,
     price: 45,
@@ -52,80 +45,28 @@ const data = [
     checkOutDate: new Date().getTime() + 1000000,
   },
   {
-    address: '671 Lincoln Ave, Winnetka, IL',
-    id: '4',
-    image:
-      'https://www.chicagotribune.com/resizer/0m2LwOyUvSRKZKbPObPuVQKRzq0=/1200x0/top/arc-anglerfish-arc2-prod-tronc.s3.amazonaws.com/public/ZCJ77ZAR7FE2VBDQOTEC24ESSI.jpg',
+    address: 'Dubai, 39205, United Arab Emirates',
+    id: '1',
     numOfBeds: 3,
     numOfGuests: 10,
-    price: 45,
-    rating: 4.4,
-    title: 'Large house for the whole family',
-    location: 'Illinois',
+    price: 60,
+    rating: 3.4,
+    title: 'Large flat for the whole family',
+    location: 'Dubai',
     description:
       'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
     checkInDate: new Date().getTime() - 1000000,
     checkOutDate: new Date().getTime() + 1000000,
   },
-
   {
-    address: 'London, 221B Baker Street',
-    id: '5',
-    image:
-      'https://thespaces.com/wp-content/uploads/2016/06/Sherlock-flat-bbc.jpg',
-    numOfBeds: 2,
-    numOfGuests: 2,
-    price: 15,
-    rating: 3.1,
+    address: 'Toronto, Golden Horseshoe, Ontario, Canada',
+    id: '1',
+    numOfBeds: 3,
+    numOfGuests: 10,
+    price: 25,
+    rating: 5,
     title: 'Small flat',
-    location: 'London',
-    description:
-      'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
-  },
-
-  {
-    address:
-      'Escalinata Marisol Parra 4 Esc. 346, Esc. 509, 22349, Baracaldo, Región de Murcia, Spain',
-    id: '6',
-    image:
-      'https://housely.com/wp-content/uploads/2016/10/ConnecticutHouse_01.jpg',
-    numOfBeds: 3,
-    numOfGuests: 6,
-    price: 30,
-    rating: 4.8,
-    title: 'Country house',
-    location: 'Baracaldo',
-    description:
-      'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
-    checkInDate: new Date().getTime() - 1000000,
-    checkOutDate: new Date().getTime() + 1000000,
-  },
-
-  {
-    address: '671 Lincoln Ave, Winnetka, IL',
-    id: '7',
-    image:
-      'https://www.chicagotribune.com/resizer/0m2LwOyUvSRKZKbPObPuVQKRzq0=/1200x0/top/arc-anglerfish-arc2-prod-tronc.s3.amazonaws.com/public/ZCJ77ZAR7FE2VBDQOTEC24ESSI.jpg',
-    numOfBeds: 3,
-    numOfGuests: 10,
-    price: 45,
-    rating: 4.4,
-    title: 'Large house for the whole family',
-    location: 'Illinois',
-    description:
-      'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
-  },
-  {
-    address: '671 Lincoln Ave, Winnetka, IL',
-    id: '8',
-    image:
-      'https://www.chicagotribune.com/resizer/0m2LwOyUvSRKZKbPObPuVQKRzq0=/1200x0/top/arc-anglerfish-arc2-prod-tronc.s3.amazonaws.com/public/ZCJ77ZAR7FE2VBDQOTEC24ESSI.jpg',
-    numOfBeds: 3,
-    numOfGuests: 10,
-    price: 45,
-    rating: 4.4,
-    title: 'Large house for the whole family',
-    location: 'Illinois',
+    location: 'Toronto',
     description:
       'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
     checkInDate: new Date().getTime() - 1000000,
